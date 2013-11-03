0a124ebe47be1e068571b64eb3888901c194a54c
Timothy Washington <twashing@gmail.com>
2013-11-03 00:55:28 -0700

    -- adding HTTP handler outline for compojure routes

--- src/stefon_compojure/handler.clj
+++ src/stefon_compojure/handler.clj
@@ -5,8 +5,33 @@
             [stefon-webui-common.core :as common]))
 
 (defroutes app-routes
+
   (GET "/helloworld" [:as req]
        (common/handle-hello-world req))
+
+
+  ;; ===> POST
+  (PUT "/post" [:as req] "put post")
+  (GET "/post" [:as req] "get post")
+  (POST "/post" [:as req] "post post")
+  (DELETE "/post" [:as req] "delete post")
+  (GET "/posts" [:as req] "get posts")
+
+  ;; ===> ASSET
+  (PUT "/asset" [:as req] "put asset")
+  (GET "/asset" [:as req] "get asset")
+  (POST "/asset" [:as req] "post asset")
+  (DELETE "/asset" [:as req] "delete asset")
+  (GET "/assets" [:as req] "get assets")
+
+  ;; ===> TAG
+  (PUT "/tag" [:as req] "put tag")
+  (GET "/tag" [:as req] "get tag")
+  (POST "/tag" [:as req] "post tag")
+  (DELETE "/tag" [:as req] "delete tag")
+  (GET "/tags" [:as req] "get tags")
+
+
   (route/resources "/")
   (route/not-found "Not Found"))
 
