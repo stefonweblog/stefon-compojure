457b33db9aeaec35438762de1eb305348d953294
Timothy Washington <twashing@gmail.com>
2013-11-23 08:07:11 -0800

    -- writing up a TODO outline for the plugin

--- test/stefon_compojure/test/plugin.clj
+++ test/stefon_compojure/test/plugin.clj
@@ -4,6 +4,53 @@
         stefon-compojure.plugin
         midje.sweet))
 
+
+
+;; TODO - hookup midje auto-test and auto-doc
+
+;; startup stefon & plugin (just memory-mode)
+
+;; basic CRUD for posts
+
+
+
+;; TODO - hookup austin
+
+;; create clojurescript that calls basic post CRUD
+
+;; clojruescript for advanced post CRUD
+
+;; clojurescript for CRUD on tags
+
+;; clojurescript for CRUD on assets
+
+;; clojurescript for CRUD on mixed content
+;;   post & tags
+;;   post & assets
+
+
+
+;; TODO - UI for weblog
+
+;; landing screen
+
+;; paging
+
+;; post replies
+;;   list replies by time
+;;   list replies by thread
+
+
+
+;; TODO - split out a theme
+
+;; TODO - documentation
+
+;; TODO - [plugin] aauthentication & admin console
+
+;; TODO - [plugin] payment gateway
+
+
 (deftest test-app
   (testing "main route"
     (let [response (app (request :get "/"))]
